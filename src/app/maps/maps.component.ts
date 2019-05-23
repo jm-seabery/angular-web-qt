acc90a40b8106829024ff2fad5d4a0fe435b27fd
jm-seabery <jm@seaberyat.com>
2019-05-23 13:23:56 +0200

    [John Murray] native to js and js to native examples

--- src/app/maps/maps.component.ts
+++ src/app/maps/maps.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
 declare const google: any;
-declare var myObj : any;
+declare var qtAccessObject : any;
+declare var $: any;
 
 interface Marker {
 lat: number;
@@ -123,11 +124,51 @@ export class MapsComponent implements OnInit {
     // To add the marker to the map, call setMap();
     marker.setMap(map);
       */  
+
+     qtAccessObject.jsStringChanged.connect(this.jsFunc);
+     qtAccessObject.frameRateStringChanged.connect(this.frameRateUpdate);
   }
 
-  public myFunction() {
+  public jsFunc(text) {
+
+    const type = ['','info','success','warning','danger'];
+
+    const color = Math.floor((Math.random() * 4) + 1);
+    const from = 'bottom';
+    const align = 'center';
+    $.notify({
+        icon: "notifications",
+        message: text
+
+    },{
+        type: type[color],
+        timer: 4000,
+        placement: {
+            from: from,
+            align: align
+        },
+        template: '<div data-notify="container" class="col-xl-4 col-lg-4 col-11 col-sm-4 col-md-4 alert alert-{0} alert-with-icon" role="alert">' +
+          '<button mat-button  type="button" aria-hidden="true" class="close mat-button" data-notify="dismiss">  <i class="material-icons">close</i></button>' +
+          '<i class="material-icons" data-notify="icon">notifications</i> ' +
+          '<span data-notify="title">{1}</span> ' +
+          '<span data-notify="message">{2}</span>' +
+          '<div class="progress" data-notify="progressbar">' +
+            '<div class="progress-bar progress-bar-{0}" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%;"></div>' +
+          '</div>' +
+          //'<a href="{3}" target="{4}" data-notify="url"></a>' +
+        '</div>'
+    });
+  }
+
+  public frameRateUpdate(text) {
+      //alert(text);
+    var el = document.getElementById("frameText");
+    el.textContent = "Framerate = " + text;
+  }
+
+  public callCPlusPlus() {
     //myObj.jscallme();
-    window["qtAccessObject"].jscallme();
+    window["qtAccessObject"].JavaScriptCallFunction('I am calling C++ from javascript');
     //alert("js call");
    } 
 }
